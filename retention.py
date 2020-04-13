8c77aa706c97afe3ac0298d901bf9250da724dca
sjtufs <sjtufs@gmail.com>
2020-04-13 04:41:22 -0400

    update retention model

--- retention.py
+++ retention.py
@@ -4,45 +4,46 @@
 # Let's try to refactor the code and parameterized the half-life predictor with pytorch.
 
 # ## Variations of Half-life regression (HLR)
-# 
+#
 # short-hand for each record \begin{align}<\cdot>&=<\Delta,x,P[\text{recall}]\in[0,1]>\\&=<\Delta,x,y\in\{0,1\}>\end{align}
-# 
+#
 # Regression against recall probability $$l_\text{recall}(<\cdot>;\theta)=(p-f_\theta(x,\Delta))^2$$
-# 
+#
 # Regression against back-solved half-life $$l_\text{half-life}(<\cdot>;\theta)=(\frac{-\Delta}{\log_2{p}}-f_\theta(x,\Delta))^2$$
-# 
+#
 # Binary recall classification $$l_\text{binary}(<\cdot>;\theta)=\text{xent}(f_\theta(x,\Delta),y)$$
-# 
+#
 # Assume that half-life increases exponentially with each repeated exposure, with a linear approximator, you get $f_\theta(x,\Delta)=2^{\theta\cdot x}$. Use this parameterization with regression against both recall probability and back-solved half-life, you get Settles' formulation:
 # $$l(<\cdot>; \theta)=(p-2^{\frac{\Delta}{2^{\theta\cdot x}}})^2+\alpha(\frac{\Delta}{\log_2(p)}-2^{\theta\cdot{x}})^2+\lambda|\theta|_2^2$$
-
-# In[1]:
 
 
 import os
 import math
-import gzip
-import copy
-import pandas as pd
+import pickle
+import argparse
 import numpy as np
+import pandas as pd
 
 import torch
-
-
-# In[2]:
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR
 
 
 MIN_HALF_LIFE = 15.0 / (24 * 60)    # 15 minutes
 MAX_HALF_LIFE = 274.                # 9 months
 LN2 = math.log(2)
 
+
 def pclip(p):
     # bound min/max model predictions (helps with loss optimization)
     return min(max(p, 0.0001), .9999)
 
+
 def hclip(h):
     # bound min/max half-life
     return min(max(h, MIN_HALF_LIFE), MAX_HALF_LIFE)
+
 
 def featurize(df):
     # disable feature by removing prefix f_
@@ -62,19 +63,21 @@ def featurize(df):
     df['bias'] = 1
     return df
 
+
 def get_featurized_df():
     featurized_df_dir = 'data/features.h5'
 
     if os.path.exists(featurized_df_dir):
         print('loading featurized_df')
         return pd.read_hdf(featurized_df_dir)
-    
+
     df = pd.read_csv('./data/settles.acl16.learning_traces.13m.csv.gz')
     df = featurize(df)
-    
+
     df.to_hdf(featurized_df_dir, 'data')
-    
+
     return df
+
 
 def get_split_dfs():
     train_df_dir = 'data/train.h5'
@@ -85,14 +88,15 @@ def get_split_dfs():
         return pd.read_hdf(train_df_dir), pd.read_hdf(test_df_dir)
 
     df = get_featurized_df()
-    
+
     splitpoint = int(0.9 * len(df))
     train_df, test_df = df.iloc[:splitpoint], df.iloc[splitpoint:]
-    
+
     train_df.to_hdf(train_df_dir, 'data')
     test_df.to_hdf(test_df_dir, 'data')
 
     return train_df, test_df
+
 
 def get_split_numpy():
     dirs = [
@@ -104,25 +108,22 @@ def get_split_numpy():
     if all(os.path.exists(d) for d in dirs):
         print('loading train test numpy')
         return (np.load(d) for d in dirs)
-    
+
     train_df, test_df = get_split_dfs()
-    
+
     feature_names = [c for c in train_df.columns if c.startswith('f_')] + ['bias']
     print('features', feature_names)
     x_train = train_df[feature_names].to_numpy().astype(np.float32)
     y_train = train_df['p_recall'].to_numpy().astype(np.float32)
     x_test = test_df[feature_names].to_numpy().astype(np.float32)
     y_test = test_df['p_recall'].to_numpy().astype(np.float32)
-    
+
     np.save(dirs[0], x_train)
     np.save(dirs[1], y_train)
     np.save(dirs[2], x_test)
     np.save(dirs[3], y_test)
-    
+
     return x_train, y_train, x_test, y_test
-
-
-# In[3]:
 
 
 class RetentionDataset(torch.utils.data.Dataset):
@@ -150,23 +151,14 @@ class RetentionDataset(torch.utils.data.Dataset):
         return torch.from_numpy(x), torch.from_numpy(y)
 
 
-# In[5]:
-
-
-train_dataset = RetentionDataset('train')
-
-
-# In[4]:
-
-
 class Net(nn.Module):
 
-    def __init__(self, n_input):
+    def __init__(self, n_input, n_output):
         super(Net, self).__init__()
         # self.fc1 = nn.Linear(n_input, 128)
         # self.dropout1 = nn.Dropout(0.25)
         # self.fc2 = nn.Linear(128, 2)
-        self.fc1 = nn.Linear(n_input, 2)
+        self.fc1 = nn.Linear(n_input, n_output)
 
     def forward(self, x):
         # x = self.fc1(x)
@@ -179,12 +171,12 @@ class Net(nn.Module):
 
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
-    loss_func = nn.CrossEntropyLoss()
+    loss_func = nn.MSELoss()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         logits = model(data)
-        loss = loss_func(logits, target)
+        loss = loss_func(logits[:, 0], target)
         loss.backward()
         optimizer.step()
         if batch_idx % args.log_interval == 0:
@@ -196,30 +188,21 @@ def train(args, model, device, train_loader, optimizer, epoch):
 def test(args, model, device, test_loader):
     model.eval()
     test_loss = 0
-    correct = 0
-    loss_func = nn.CrossEntropyLoss(reduction='sum')
+    loss_func = nn.MSELoss(reduction='mean')
     predictions = []
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
-            logits = model(data)
+            logits = model(data)[:, 0]
             # sum up batch loss
             test_loss += loss_func(logits, target).item()
             # get the index of the max log-probability
-            pred = logits.argmax(dim=1, keepdim=True)
-            correct += pred.eq(target.view_as(pred)).sum().item()
-            predictions += pred[:, 0].detach().cpu().numpy().tolist()
+            predictions += logits.detach().cpu().numpy().tolist()
 
     test_loss /= len(test_loader.dataset)
 
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+    print('\nTest set: Average loss: {:.4f}\n'.format(test_loss))
     return test_loss, predictions
-
-
-# In[ ]:
-
 
 
 def main():
@@ -257,8 +240,11 @@ def main():
         batch_size=args.test_batch_size, shuffle=False, **kwargs)
 
     n_input = train_dataset.x.shape[1]
-    model = Net(n_input=n_input).to(device)
+    model = Net(n_input=n_input, n_output=1).to(device)
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
+
+    checkpoint_dir = "checkpoints/retention_model.pt"
+    prediction_dir = "checkpoints/predictions.pkl"
 
     best_test_loss = 9999
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
@@ -267,14 +253,13 @@ def main():
         test_loss, predictions = test(args, model, device, test_loader)
         scheduler.step()
         if test_loss < best_test_loss:
-            checkpoint_dir = "checkpoints/retention_model.pt"
             torch.save(model.state_dict(), checkpoint_dir)
             print('save model checkpoint to', checkpoint_dir)
+            with open(prediction_dir, 'wb') as f:
+                pickle.dump(predictions, f)
+            print('save predictions to', prediction_dir)
             best_test_loss = test_loss
 
 
-# In[ ]:
-
-
-
-
+if __name__ == '__main__':
+    main()
